bf6dc339ccd80dbb189bc49265b73365b6019c2d
Rupesh-stack <60387610+Rupesh-stack@users.noreply.github.com>
2024-08-28 11:34:03 +0530

    get users need to token

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
-router.get('/users',userController.getUsers);
+router.get('/users',authenticateJWT,userController.getUsers);
 router.get('/users/:id', userController.getUserById);
 router.put('/users/:id', userController.updateUser);
 router.delete('/users/:id', userController.deleteUser);
